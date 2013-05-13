738139bf979de7d2b378fe8b4dc422c036600cdd
Max Kolysh <maxko87@gmail.com>
2013-05-13 00:27:54 -0400

    correlations in idea 3

--- idea_3.py
+++ idea_3.py
@@ -5,9 +5,21 @@ the edge weights are the distance between two businesses
 
 import load_data
 import math, numpy as np
+from helpers import *
+
 #1 degree of latitude is approx 69 miles
-n = 5
-businesses = load_data.load_objects("business", n)
+n = 500
+pre_businesses = load_data.load_objects("business", n)
+
+businesses=[]
+for b in pre_businesses:
+  if 'Restaurants' in b.categories:
+    businesses += [b]
+
+n = len(businesses)
+for b in businesses:
+  print "rating for ",str(b), ":", b.stars, ", # reviews: ", b.review_count
+
 #users = load_data.load_objects("user", 5000)
 #reviews = load_data.load_objects("review", 50000)
 A = []
@@ -24,17 +36,11 @@ for i in range(n):
     long_dist = bus.longitude-bus2.longitude
     latlongdist = math.sqrt(math.pow(lat_dist,2) + math.pow(long_dist,2))
     dists.append(1/latlongdist)
-  print i
   A.append(dists)
-print max(A[1])
 #makes the matrix row stochastic (rows sum to 1)
-for i in range(n):
-  s = sum(A[i])
-  A[i][0:] = [x/s for x in A[i][0:]]
-print A
-b = [0]*n
-b[1] = 1
-print b
+#for i in range(n):
+  #s = sum(A[i])
+  #A[i][0:] = [x/s for x in A[i][0:]]
 #b = A*b
 '''
 for x in range(10):
@@ -46,4 +52,17 @@ print b
 
 eigenvalues, eigenvectors = np.linalg.eig(A)
 print eigenvalues
-print eigenvectors
+rankings  = []
+for row in eigenvectors:
+  rankings += [row[0]]
+
+stars_cor = []
+review_count_corr = []
+
+for i in range(len(businesses)):
+  stars_cor += [businesses[i].stars]
+  review_count_corr += [businesses[i].review_count]
+
+
+print ("stars correlation: " + str(correlation(rankings, stars_cor)))
+print ("review count correlation: " + str(correlation(rankings, review_count_corr)))
